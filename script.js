2dd00e63ca25d37c7e0d23b7de0cd3a3d6b9e1a3
sanjanaak556 <117841426+sanjanaak556@users.noreply.github.com>
2023-05-30 13:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-let players = ["Player1", "Player2", "Player3"];
-let person = { name: "John", age: 25 };
+let players = ["John", "Bob", "Alice", "Poppy"];
+let person = { name: "John Doe", age: 80 };
 
 // Creating a reference to the players array
 let team = players;
 
 // Creating a copy of the players array
-let team1 = [...players];
+let team1 = [team1];
 
 // Creating a copy of the person object
-let cap1 = { ...person };
+let cap1 = {cap1 };
 
 window.players = players;
 window.person = person;
